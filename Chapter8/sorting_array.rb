e0d9ce58ca440550b9fbcdec01e019d4569cb215
Sepand Assadi <info@sepandassadi.com>
2016-01-23 18:32:37 -0800

    8.1 sorting array

---
+++ Chapter8/sorting_array.rb
@@ -0,0 +1,14 @@
+def sorting_array
+  puts "Give me some words, and I will sort them: "
+  words = []
+  while true
+    word = gets.chomp
+    if word == ''
+      break
+    end
+    words.push word
+  end
+    puts "Here they are, sorted: "
+    puts words.sort
+end
+sorting_array
